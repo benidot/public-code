e35fc3f459bf6b0e6bc1be77a0f12eb1aca7832a
kat <193017@upf.br>
2025-08-05 02:38:22 -0300

    i am unemployed

---
+++ c++ practice/p04.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double numba;
+
+    for (int i = 0; i <= 5; i++) {
+        cout << endl;
+    }
+
+    cout << "plug a numba here >> "; cin >> numba;
+    cout << "yo numba times too is " << numba * 2 << endl;
+
+}
---
+++ c++ practice/p05.cpp
@@ -0,0 +1,15 @@
+// little pyramid
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for(int j = 0; j <= 5; j++) {
+        for (int i = 0; i <= j; i++) {
+            cout << "● ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
