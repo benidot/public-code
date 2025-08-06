84940b2c56903b697f06764da8b02ccdb6619cbf
kat <193017@upf.br>
2025-08-06 02:23:10 -0300

    I LOVE JULIA

---
+++ julia practice/p01.jl
@@ -0,0 +1,4 @@
+# THANK YOU FIRESHIP I FUCKING LOVE THIS LANGUAGE FROM THE LITTLE I KNOW OF IT
+
+print("wat yo name: ")
+print("hello, ", readline(), "\n")
---
+++ julia practice/p02.jl
@@ -0,0 +1,18 @@
+# iterate over string
+
+for i in 1:15
+    println()
+end
+
+print("gimme a string: ")
+string = readline()
+for i in 1:15
+    println()
+end
+print("  ")
+for i in 1:length(string)
+    print(string[i])
+    sleep(0.05)
+end
+
+println()
